3719aee1f847b7db603d53d4c43e564a1629528a
AkbarZul <akbar.zul73@gmail.com>
2021-01-16 21:45:06 +0700

    fix bug getAddressByUserId

--- src/models/addressMdl.js
+++ src/models/addressMdl.js
@@ -30,7 +30,7 @@ module.exports = {
 
     getAddressByUser: (user_id) => {
         return new Promise((resolve, reject) => {
-            const queryString = "SELECT a.id, a.fullname, a.address, a.city, a.zip_code, a.country, u.id FROM address_customer AS a JOIN users AS u on u.id = a.user_id"
+            const queryString = "SELECT a.id, a.fullname, a.address, a.city, a.zip_code, a.country, u.id FROM address_customer AS a JOIN users AS u on u.id = a.user_id WHERE user_id = " + user_id;
             db.query(queryString, user_id, (err, data) => {
                 if(!err) {
                     resolve(data)
